8def2dc6e023de88a62d07a20e94d989eb185a85
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-12 17:52:27 -0500

    Revert "Merge branch 'main' of https://github.com/TammyXie4677/ReservationSystem"
    This reverts commit 48b0f3f90a847c80cdd06f49194ae7c26c3ee793, reversing
    changes made to 38df6673f0587db7622049a8f02347c427c4fb0f.

--- RestaurantReservationSystem/Program.cs
+++ RestaurantReservationSystem/Program.cs
@@ -1,15 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<RestaurantBookingContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 var app = builder.Build();
 
