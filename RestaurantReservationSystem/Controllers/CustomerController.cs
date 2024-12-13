43b308b80433a0ac641cea59721b00e3a7df3bd3
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-13 17:46:05 -0500

    Update CustomerController.cs

--- RestaurantReservationSystem/Controllers/CustomerController.cs
+++ RestaurantReservationSystem/Controllers/CustomerController.cs
@@ -1,14 +1,37 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RestaurantReservationSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservationSystem.ViewModels;
+using System.Linq;
 
 namespace RestaurantReservationSystem.Controllers
 {
     [Authorize(Roles = "Customer")]
     public class CustomerController : Controller
     {
+        private readonly RestaurantReservationContext _context;
+
+        public CustomerController(RestaurantReservationContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult CustomerDashboard()
         {
-            return View();
+            // Fetch the list of available restaurants from the database
+            var restaurants = _context.Restaurants
+                .Select(r => new RestaurantViewModel
+                {
+                    RestaurantId = r.RestaurantId,
+                    Name = r.Name,
+                    CuisineType = r.CuisineType,
+                    PriceRange = r.PriceRange
+                })
+                .ToList();
+
+            // Pass the list of restaurants to the view
+            return View(restaurants);
         }
     }
 }
