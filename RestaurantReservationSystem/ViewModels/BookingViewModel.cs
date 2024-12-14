8e4e90be8a4791dbabff052eea9db3a3e0112c24
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-14 01:16:22 -0500

    Update BookingViewModel.cs

--- RestaurantReservationSystem/ViewModels/BookingViewModel.cs
+++ RestaurantReservationSystem/ViewModels/BookingViewModel.cs
@@ -6,17 +6,28 @@ namespace RestaurantReservationSystem.ViewModels
     {
         public int BookingId { get; set; }
 
-        public int UserId { get; set; }
-
-        public int RestaurantId { get; set; }
-
+        [Required(ErrorMessage = "First Name is required.")]
         public string FirstName { get; set; } = null!;
 
+        [Required(ErrorMessage = "Last Name is required.")]
         public string LastName { get; set; } = null!;
 
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = null!;
+
+        // Phone number is required with format validation
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^\(\d{3}\) \d{3}-\d{4}$", ErrorMessage = "Phone number must be in the format (xxx) xxx-xxxx.")]
+        public string? PhoneNumber { get; set; }
+
+        // BookingDate is required
+        [Required(ErrorMessage = "Booking Date is required.")]
         public DateTime BookingDate { get; set; }
 
-        [Range(1, 20)] 
+        // GuestsCount is required with a range
+        [Required(ErrorMessage = "Number of guests is required.")]
+        [Range(1, 20, ErrorMessage = "Number of guests must be between 1 and 20.")]
         public int GuestsCount { get; set; }
     }
 }
